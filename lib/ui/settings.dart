e6bd82a3df787a399ec0c1d8739b401c40d9ba2a
jsofosu <61045983+jsofosu@users.noreply.github.com>
2021-05-14 14:09:01 +0000

    Update settings.dart

--- lib/ui/settings.dart
+++ lib/ui/settings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/bottomnavigation.dart';
+import '../widgets/textwidget.dart';
 
 class Settings extends StatelessWidget {
   @override
@@ -178,24 +179,6 @@ class SettingsCard extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class Texxt extends StatelessWidget {
-  final String text;
-  final Color color;
-  final double size;
-  final double height;
-  final FontWeight weight;
-
-  Texxt({this.text, this.color, this.size, this.height, this.weight});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(color: color, fontSize: size, fontWeight: weight),
     );
   }
 }
